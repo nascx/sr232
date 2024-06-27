cb3d92165e7074319511b8a4dd8b77382cd6c0d4
nascx <nascx@gmail.com>
2024-06-27 14:19:50 -0400

    teste

--- server/src/controller/sendResults.ts
+++ server/src/controller/sendResults.ts
@@ -5,13 +5,17 @@ const sendResults = (req: Request, res: Response) => {
 
    const { registration, name, result } = req.body
 
+   console.log({ registration, name, result })
+
    const date = new Date().toLocaleDateString('pt-br')
 
    try {
       const response = sendResultsTestsToDB(registration, name, date, result)
       res.status(200).json(response)
+      console.log(response)
    } catch (error) {
       console.log('Erro ao salvar no banco - Controller')
+      console.log(error)
       res.status(400).json(error)
    }
 }
